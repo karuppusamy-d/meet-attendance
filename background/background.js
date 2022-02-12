d787f99821e9da0f8455de5cf3d787f87cd6309a
Karuppusamy <d.karuppusamy@outlook.com>
2022-02-13 03:35:18 +0530

    Format documents

--- background/background.js
+++ background/background.js
@@ -1,90 +1,100 @@
 // For Declearative Content
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: 'developer.chrome.com' },
-        })
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+  chrome.declarativeContent.onPageChanged.addRules([
+    {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: "developer.chrome.com" },
+        }),
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()],
+    },
+  ]);
 });
 
 // Event Listener
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log("Data Received", request);
-    if (request.dist === "background") {
-        createDocument(request.dataValues, request.participantNames, request.timeValues, request.meetingId);
-        sendResponse("Received by background script");
-    }
-    else if (request.dist === "content") {
-        // To send back your response  to the current tab
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, request, function (response) {
-                console.log(response);
-            });
-        })
-        sendResponse("Received by background script");
-    }
+  console.log("Data Received", request);
+  if (request.dist === "background") {
+    createDocument(
+      request.dataValues,
+      request.participantNames,
+      request.timeValues,
+      request.meetingId
+    );
+    sendResponse("Received by background script");
+  } else if (request.dist === "content") {
+    // To send back your response  to the current tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, request, function (response) {
+        console.log(response);
+      });
+    });
+    sendResponse("Received by background script");
+  }
 });
-
 
 // function for HTML Creation
 async function createDocument(dataValues, key, timeValues, meetingId) {
-    var template = "";
-    let now = new Date();
-    let currentTime = now.getHours() + ':' + (now.getMinutes().toString());
-    let currentDate = now.getFullYear() + '-' + (now.getMonth() + 1).toString() + '-' + now.getDate().toString();
+  var template = "";
+  let now = new Date();
+  let currentTime = now.getHours() + ":" + now.getMinutes().toString();
+  let currentDate =
+    now.getFullYear() +
+    "-" +
+    (now.getMonth() + 1).toString() +
+    "-" +
+    now.getDate().toString();
 
-    var thead = "";
-    var tbody = "";
+  var thead = "";
+  var tbody = "";
 
-    // Time Value Header Create
-    for (let el of timeValues) {
-        thead += '<th>' + el + '</th>\n';
+  // Time Value Header Create
+  for (let el of timeValues) {
+    thead += "<th>" + el + "</th>\n";
+  }
+
+  // Data Value Create
+  for (let el of key) {
+    let sn = "<td>" + (key.indexOf(el) + 1) + "</td>";
+    let name = "<td>" + el + "</td>";
+    let t = dataValues[el];
+    let tdata = "";
+    for (let i of timeValues) {
+      if (t.includes(i)) {
+        tdata += '<td class="present">' + "<p>P</p>" + "</td>";
+      } else {
+        tdata += '<td class="absent">' + "A" + "</td>";
+      }
     }
+    tbody += "<tr>" + sn + name + tdata + "</tr>";
+  }
 
-    // Data Value Create
-    for (let el of key) {
-        let sn = '<td>' + (key.indexOf(el) + 1) + '</td>';
-        let name = '<td>' + el + '</td>';
-        let t = dataValues[el];
-        let tdata = "";
-        for (let i of timeValues) {
-            if (t.includes(i)) {
-                tdata += '<td class="present">' + "<p>P</p>" + '</td>';
-            }
-            else {
-                tdata += '<td class="absent">' + "A" + '</td>';
-            }
-        }
-        tbody += '<tr>' + sn + name + tdata + '</tr>';
-    }
+  template = await getTemplate;
 
-    template = await getTemplate;
+  template = template.replace("[%%title%%]", currentDate + " " + currentTime);
+  template = template.replace("[%%date%%]", currentDate);
+  template = template.replace("[%%time%%]", currentTime);
+  template = template.replace("[%%meetID%%]", meetingId);
+  template = template.replace("[%%tableHead%%]", thead);
+  template = template.replace("[%%tableBody%%]", tbody);
 
-    template = template.replace('[%%title%%]', (currentDate + " " + currentTime));
-    template = template.replace('[%%date%%]', currentDate);
-    template = template.replace('[%%time%%]', currentTime);
-    template = template.replace('[%%meetID%%]', meetingId);
-    template = template.replace('[%%tableHead%%]', thead);
-    template = template.replace('[%%tableBody%%]', tbody);
-
-    filename = "attendance_" + currentDate + ".html";
-    var blob = new Blob([template], { type: 'text/html;charset=utf-8' });
-    let url = URL.createObjectURL(blob);
-    chrome.downloads.download({
-        url: url,
-        filename: filename
-    });
+  filename = "attendance_" + currentDate + ".html";
+  var blob = new Blob([template], { type: "text/html;charset=utf-8" });
+  let url = URL.createObjectURL(blob);
+  chrome.downloads.download({
+    url: url,
+    filename: filename,
+  });
 }
 
 const getTemplate = new Promise((resolve, reject) => {
-    var client = new XMLHttpRequest();
-    client.open('GET', '/background/template.html');
-    client.onreadystatechange = _ => {
-        if (client.readyState === 4) {
-            resolve(client.responseText);
-        }
+  var client = new XMLHttpRequest();
+  client.open("GET", "/background/template.html");
+  client.onreadystatechange = (_) => {
+    if (client.readyState === 4) {
+      resolve(client.responseText);
     }
-    client.send();
-})
+  };
+  client.send();
+});
--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,158 +1,161 @@
 // Global Variables
-var dataStorage = {};       // Data Variable
-var participantNames = [];   // For Key Storage
-var timeStamp = [];         // For Time Storage
-var interval_id;            // For start and stop Monitoring
-var meetingId;             // For storing Meeting ID
+var dataStorage = {}; // Data Variable
+var participantNames = []; // For Key Storage
+var timeStamp = []; // For Time Storage
+var interval_id; // For start and stop Monitoring
+var meetingId; // For storing Meeting ID
 
 // For Push to Talk Feature
 $(document).keydown(function (event) {
-    // var keycode = (event.keyCode ? event.keyCode : event.which);
-    if (event.which == 32) {
-        if (event.ctrlKey) {
-            $("div[data-tooltip*='camera']").click();
-        }
-        else {
-            $("div[data-tooltip*='microphone']").click();
-        }
+  // var keycode = (event.keyCode ? event.keyCode : event.which);
+  if (event.which == 32) {
+    if (event.ctrlKey) {
+      $("div[data-tooltip*='camera']").click();
+    } else {
+      $("div[data-tooltip*='microphone']").click();
     }
+  }
 });
 
 // Function to Fetch List of Participants
 function getListOfParticipants() {
-    let data = [];
-    for (let i of $("[data-participant-id], [data-requested-participant-id]")) {
-        let name = i.outerText || i.innerText;
-        // Skip if Name is You
-        if (name == "You") {
-            continue;
-        }
-        // Remove Unnecessary things from name
-        name = name.replace('\n', '');
-        name = name.replace('Hide Participant', '');
-        name = name.trim();
-        data.push(name);
+  let data = [];
+  for (let i of $("[data-participant-id], [data-requested-participant-id]")) {
+    let name = i.outerText || i.innerText;
+    // Skip if Name is You
+    if (name == "You") {
+      continue;
     }
-    return data;
+    // Remove Unnecessary things from name
+    name = name.replace("\n", "");
+    name = name.replace("Hide Participant", "");
+    name = name.trim();
+    data.push(name);
+  }
+  return data;
 }
 
 // Function to Log Participants data with TimeStamp
 function logParticipantsData() {
-    let now = new Date();
-    let currentTime = now.getHours() + ':' + now.getMinutes().toString();
-    let data = getListOfParticipants(); // Returns current Name List of Participants
-    console.log(data);
+  let now = new Date();
+  let currentTime = now.getHours() + ":" + now.getMinutes().toString();
+  let data = getListOfParticipants(); // Returns current Name List of Participants
+  console.log(data);
 
-    // Loops through Participants list 
-    for (let name of data) {
-        let time = dataStorage[name] || [];
-        time.push(currentTime);
-        dataStorage[name] = time;
-        if (!participantNames.includes(name)) {
-            participantNames.push(name);
-        }
+  // Loops through Participants list
+  for (let name of data) {
+    let time = dataStorage[name] || [];
+    time.push(currentTime);
+    dataStorage[name] = time;
+    if (!participantNames.includes(name)) {
+      participantNames.push(name);
     }
-    timeStamp.push(currentTime);
-    console.log(dataStorage);
+  }
+  timeStamp.push(currentTime);
+  console.log(dataStorage);
 }
 
 // Function to get Meeting ID
 function getMeetingId() {
-    let id = window.location.href;
-    id = id.split('/')[3];
-    if (id === "") {
-        console.log("Not a Meeting");
-        return false;
-    }
-    if (meetingId !== id) {
-        clearData();
-        console.log("New Meeting");
-    }
-    meetingId = id;
-    return true;
+  let id = window.location.href;
+  id = id.split("/")[3];
+  if (id === "") {
+    console.log("Not a Meeting");
+    return false;
+  }
+  if (meetingId !== id) {
+    clearData();
+    console.log("New Meeting");
+  }
+  meetingId = id;
+  return true;
 }
 
 // Function to Start Monitoring
 function startMonitoring(time = 300000) {
-    stopMonitoring();
-    // getMeetingId returns false if not a meeting
-    if (!getMeetingId()) {
-        console.log("Not Starting Service. Because it is not a Meeting.");
-        return false;
-    }
-    logParticipantsData(); // Logs data on Start
-    interval_id = setInterval(function () {
-        logParticipantsData(); // Logs data on Specific Intervel
-    }, time);
-    console.log('started');
+  stopMonitoring();
+  // getMeetingId returns false if not a meeting
+  if (!getMeetingId()) {
+    console.log("Not Starting Service. Because it is not a Meeting.");
+    return false;
+  }
+  logParticipantsData(); // Logs data on Start
+  interval_id = setInterval(function () {
+    logParticipantsData(); // Logs data on Specific Intervel
+  }, time);
+  console.log("started");
 }
 
 // Function to Stop Monitoring
 function stopMonitoring() {
-    if (!(interval_id == undefined)) {
-        clearInterval(interval_id);
-        interval_id = undefined;
-    }
-    console.log('stoped');
+  if (!(interval_id == undefined)) {
+    clearInterval(interval_id);
+    interval_id = undefined;
+  }
+  console.log("stoped");
 }
 
 // Function to send data to Background script
 function sendData() {
-    chrome.runtime.sendMessage({ dist: "background", dataValues: dataStorage, participantNames: participantNames, timeValues: timeStamp, meetingId: meetingId }, res => {
-        console.log(res);
-    });
-    console.log('data sent');
+  chrome.runtime.sendMessage(
+    {
+      dist: "background",
+      dataValues: dataStorage,
+      participantNames: participantNames,
+      timeValues: timeStamp,
+      meetingId: meetingId,
+    },
+    (res) => {
+      console.log(res);
+    }
+  );
+  console.log("data sent");
 }
 
 // Function to clear dataStorage
 function clearData() {
-    dataStorage = {};
-    participantNames = [];
-    timeStamp = [];
-    console.log('cleared');
+  dataStorage = {};
+  participantNames = [];
+  timeStamp = [];
+  console.log("cleared");
 }
-
 
 // Event Listiner
 chrome.runtime.onMessage.addListener((request, sender, response) => {
-    if (request.dist === "content") {
-        console.log(request);
-        if (getMeetingId()) {
-            let action = request.action;
-            if (action === "start") {
-                let delay = request.delay;
-                delay = parseInt(delay);
-                if (isNaN(delay)) {
-                    delay = 5;
-                }
-                let t_mil = delay * 60000;
+  if (request.dist === "content") {
+    console.log(request);
+    if (getMeetingId()) {
+      let action = request.action;
+      if (action === "start") {
+        let delay = request.delay;
+        delay = parseInt(delay);
+        if (isNaN(delay)) {
+          delay = 5;
+        }
+        let t_mil = delay * 60000;
 
-                startMonitoring(t_mil);
-                sendResponse("Started");
-            }
-            else if (action === "stop") {
-                stopMonitoring();
-                sendResponse("Stopped");
-            }
-            else if (action === "save") {
-                sendData();
-                sendResponse("Downloading");
-            }
-            else if (action == "clear") {
-                clearData();
-                sendResponse("Cleared");
-            }
-        }
-        else {
-            sendResponse("Not a Meeting");
-        }
-        response("Received by Content Script");
+        startMonitoring(t_mil);
+        sendResponse("Started");
+      } else if (action === "stop") {
+        stopMonitoring();
+        sendResponse("Stopped");
+      } else if (action === "save") {
+        sendData();
+        sendResponse("Downloading");
+      } else if (action == "clear") {
+        clearData();
+        sendResponse("Cleared");
+      }
+    } else {
+      sendResponse("Not a Meeting");
     }
+    response("Received by Content Script");
+  }
 });
 
 // Function to send data to Popup
 function sendResponse(data) {
-    chrome.runtime.sendMessage({ dist: "popup", data: data }, (res) => {
-        console.log(res);
-    });
+  chrome.runtime.sendMessage({ dist: "popup", data: data }, (res) => {
+    console.log(res);
+  });
 }
